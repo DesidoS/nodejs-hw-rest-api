d1ea82dac73b1072286ed16dc80d4b1826340540
Oleksii Kravchenko <100207277+DesidoS@users.noreply.github.com>
2023-01-12 22:35:01 +0200

    add hw-03

---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+const tryCatchWrapper = require("./tryCatchWrapper");
+const validationContact = require("./validation");
+const validationFavorite = require("./validationFavorite");
+
+module.exports = {
+  validationContact,
+  tryCatchWrapper,
+  validationFavorite,
+};
